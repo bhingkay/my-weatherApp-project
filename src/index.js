f07888c021787b6ec31a34cc03c882417fd9cc1e
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-02-15 11:41:56 +0200

    Moved my existing files (index.html, styles.css and index.js)

---
+++ src/index.js
@@ -0,0 +1,52 @@
+function showDetails(response) {
+  console.log(response);
+  let showCityElement = document.querySelector("#show-city");
+  let showUnitElement = document.querySelector("#unit");
+  let showDetails = document.querySelector("#show-detail");
+  let showHumidity = document.querySelector("#show-humidity");
+  let showWind = document.querySelector("#show-wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+
+  showCityElement.innerHTML = response.data.name;
+  showUnitElement.innerHTML = Math.round(response.data.main.temp);
+  showDetails.innerHTML = response.data.weather[0].description;
+  showHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  showWind.innerHTML = `Wind: ${windSpeed} km/h`;
+}
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = cityInput.value;
+
+  if (city !== "") {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let units = "metric";
+    let city = cityInput.value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showDetails);
+  } else {
+    alert("Please type a city...");
+  }
+}
+
+function showLocation(position) {
+  console.log(position);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  axios.get(url).then(showDetails);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+let searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener("click", search);
+
+let currentBtn = document.querySelector("#current-location-button");
+currentBtn.addEventListener("click", getLocation);
