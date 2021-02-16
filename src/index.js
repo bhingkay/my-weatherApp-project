22801afa0ea8db3a3d0fd9ac81b484c241e8a53a
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-02-16 11:54:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ function searchCity(city) {
   if (city !== "") {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let units = "metric";
-    let city = cityInput.value;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
     axios.get(apiUrl).then(showDetails);
